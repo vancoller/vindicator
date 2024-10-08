21203ecbacab8f158d8a5372928333581d3eec44
Phillip van Coller <phillip@vancoller.com>
2024-10-08 20:07:35 +0200

    more huge changes I don't want to talk about

--- Vindicator.Service/Models/VindicatorSettings.cs
+++ Vindicator.Service/Models/VindicatorSettings.cs
@@ -5,7 +5,6 @@ namespace Vindicator.Service.Models
     public class VindicatorSettings
     {
         public double TakeProfitPips { get; set; }
-        public double TakeProfitMoney { get; set; }
         public int PipsBetweenTrades { get; set; }
         public bool GenerateBacktestReport { get; set; }
         public int StartRecoveryPips { get; set; }
